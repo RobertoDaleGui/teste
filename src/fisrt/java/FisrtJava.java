b37193b2175e565dc597313b2e7eb778de9487e1
RobertoDalGui <roberto.guimaraes@credihome.com.br>
2021-01-12 13:35:49 -0200

    First commit

---
+++ src/fisrt/java/FisrtJava.java
@@ -0,0 +1,13 @@
+package fisrt.java;
+
+public class FisrtJava {
+
+    public static void main(String[] args) {
+        int variavel;
+        variavel = 1;
+        final int variavel2 = 1;
+        variavel2 = 2;
+        System.out.println("Logando algo");
+    }
+
+}
